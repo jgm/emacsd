dd3f991fb202b210e4d620338be96c730314b574
John MacFarlane <jgm@berkeley.edu>
2012-12-14 10:23:52 -0800

    2-space indent for js2-mode.

--- init.el
+++ init.el
@@ -180,7 +180,7 @@
 (add-to-list 'load-path "~/.emacs.d/js2-mode")
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(setq js-indent-level 2)
+(setq js2-basic-offset 2)
 
 ;;; Go
 (require 'go-mode-load)
