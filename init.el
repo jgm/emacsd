b70a8b5aaa0308765062f435471c930ea6762180
John MacFarlane <jgm@berkeley.edu>
2012-12-14 09:40:28 -0800

    Fixed typo.

--- init.el
+++ init.el
@@ -177,7 +177,7 @@
 (add-to-list 'auto-mode-alist '("\\.rs$" . rust-mode))
 
 ;;; Javascript
-(add-to-load-path "~/.emacs.d/js2-mode")
+(add-to-list 'load-path "~/.emacs.d/js2-mode")
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 (setq js-indent-level 2)
