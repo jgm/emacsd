d25e14549e617dfc7ab1f2bf128d7dc396ab2695
John MacFarlane <jgm@berkeley.edu>
2014-08-24 21:24:07 -0700

    More haskell mode improvements (key bindings etc.)
    From
    https://github.com/serras/emacs-haskell-tutorial/blob/master/tutorial.md

--- init.el
+++ init.el
@@ -1,6 +1,7 @@
 (setq my-packages
    '(use-package evil evil-leader evil-jumper deft markdown-mode
-     magit full-ack yasnippet js2-mode phi-rectangle haskell-mode))
+     magit full-ack yasnippet js2-mode phi-rectangle haskell-mode
+     ghc))
 
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -207,18 +208,41 @@
 
 ;;; Haskell
 (use-package haskell-mode)
+(defun start-ghc-mod ()
+  (interactive)
+  (autoload 'ghc-init "ghc" nil t)
+  (autoload 'ghc-debug "ghc" nil t)
+  (ghc-init))
 (add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
 (add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
 ;;(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 (add-hook 'haskell-mode-hook 'font-lock-mode)
 (global-set-key [(control meta down-mouse-3)] 'imenu)
-(eval-after-load 'haskell-mode
-  '(define-key haskell-mode-map [f8] 'haskell-navigate-imports))
 (setenv "PATH" (concat "~/.cabal/bin:~/Library/Haskell/bin:" (getenv "PATH")))
-(add-to-list 'exec-path "~/.cabal/bin:~/Library/Haskell/bin:")
-(custom-set-variables '(haskell-tags-on-save t))
-;; Interaction with inferior Haskell interpreter: just hit C-c C-z or C-c C-l.
+(add-to-list 'exec-path "~/.cabal/bin")
+(add-to-list 'exec-path "~/Library/Haskell/bin")
+
+(custom-set-variables
+  '(haskell-process-suggest-remove-import-lines t)
+  '(haskell-process-auto-import-loaded-modules t)
+  '(haskell-tags-on-save t)
+  '(haskell-process-log t))
+
+(eval-after-load 'haskell-mode '(progn
+  (define-key haskell-mode-map [f8] 'haskell-navigate-imports)
+  (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+  (define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-mode-map (kbd "C-c C-n C-t") 'haskell-process-do-type)
+  (define-key haskell-mode-map (kbd "C-c C-n C-i") 'haskell-process-do-info)
+  (define-key haskell-mode-map (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+  (define-key haskell-mode-map (kbd "C-c C-n c") 'haskell-process-cabal)
+  (define-key haskell-mode-map (kbd "SPC") 'haskell-mode-contextual-space)))
+(eval-after-load 'haskell-cabal '(progn
+  (define-key haskell-cabal-mode-map (kbd "C-`") 'haskell-interactive-bring)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-k") 'haskell-interactive-ode-clear)
+  (define-key haskell-cabal-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+  (define-key haskell-cabal-mode-map (kbd "C-c c") 'haskell-process-cabal)))
 
 ;;; OCaml
 ;; (add-to-list 'load-path "~/.emacs.d/tuareg-2.0.4")
