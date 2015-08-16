38244024d0dd34d87ac068f6fa54c43d60ff389b
John MacFarlane <jgm@berkeley.edu>
2015-08-16 11:16:52 -0700

    Fixed melpa URL.

--- init.el
+++ init.el
@@ -6,7 +6,7 @@
 (when (>= emacs-major-version 24)
   (require 'package)
   (add-to-list 'package-archives
-               '("melpa" . "http://melpa.milkbox.net/packages/") t)
+               '("melpa" . "http://melpa.org/packages/") t)
   (package-initialize)
   ;; Fetch packages from melpa/elpa if not present:
   (defvar refreshed nil)
